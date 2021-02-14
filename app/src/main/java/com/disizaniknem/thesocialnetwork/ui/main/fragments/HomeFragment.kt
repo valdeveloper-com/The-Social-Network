2fc4c5515951f6b2eb801fe6252b87fe0fa649a7
disizaniknem <disizaniknem@gmail.com>
2021-02-14 23:09:47 +0100

    HomeFragment + BasePostFragment

--- app/src/main/java/com/disizaniknem/thesocialnetwork/ui/main/fragments/HomeFragment.kt
+++ app/src/main/java/com/disizaniknem/thesocialnetwork/ui/main/fragments/HomeFragment.kt
@@ -1,7 +1,36 @@
 package com.disizaniknem.thesocialnetwork.ui.main.fragments
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.disizaniknem.thesocialnetwork.R
+import com.disizaniknem.thesocialnetwork.ui.main.viewmodels.BasePostViewModel
+import com.disizaniknem.thesocialnetwork.ui.main.viewmodels.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+@AndroidEntryPoint
+class HomeFragment : BasePostFragment(R.layout.fragment_home) {
+
+    override val postProgressBar: ProgressBar
+        get() = allPostsProgressBar
+
+    override val basePostViewModel: BasePostViewModel
+        get() {
+            val vm: HomeViewModel by viewModels()
+            return vm
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() = rvAllPosts.apply {
+        adapter = postAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+        itemAnimator = null
+    }
 }
---
+++ app/src/main/java/com/disizaniknem/thesocialnetwork/ui/main/viewmodels/HomeViewModel.kt
@@ -0,0 +1,36 @@
+package com.disizaniknem.thesocialnetwork.ui.main.viewmodels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.disizaniknem.thesocialnetwork.data.entities.Post
+import com.disizaniknem.thesocialnetwork.other.Event
+import com.disizaniknem.thesocialnetwork.other.Resource
+import com.disizaniknem.thesocialnetwork.repositories.MainRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class HomeViewModel @ViewModelInject constructor(
+    private val repository: MainRepository,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Main
+) : BasePostViewModel(repository, dispatcher) {
+
+    private val _posts = MutableLiveData<Event<Resource<List<Post>>>>()
+
+    override val posts: LiveData<Event<Resource<List<Post>>>>
+        get() = _posts
+
+    init {
+        getPosts()
+    }
+
+    override fun getPosts(uid: String) {
+        _posts.postValue(Event(Resource.Loading()))
+        viewModelScope.launch(dispatcher) {
+            val result = repository.getPostsForFollows()
+            _posts.postValue(Event(result))
+        }
+    }
+}
