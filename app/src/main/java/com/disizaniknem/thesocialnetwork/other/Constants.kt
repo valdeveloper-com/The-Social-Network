470e184c28595be184218b236f66a11bd4ad0483
disizaniknem <disizaniknem@gmail.com>
2021-02-14 17:52:36 +0100

    Repository pour l'enregistrement d'un utilisateur

---
+++ app/src/main/java/com/disizaniknem/thesocialnetwork/other/Constants.kt
@@ -0,0 +1,5 @@
+package com.disizaniknem.thesocialnetwork.other
+
+object Constants {
+    const val DEFAULT_PROFILE_PICTURE_URL = "https://firebasestorage.googleapis.com/v0/b/the-social-network-8166b.appspot.com/o/img_avatar.png?alt=media&token=9c793ead-e2c0-43d1-aea1-7fdaa26e7cdf"
+}
--- app/src/main/java/com/disizaniknem/thesocialnetwork/repositories/DefaultAuthRepository.kt
+++ app/src/main/java/com/disizaniknem/thesocialnetwork/repositories/DefaultAuthRepository.kt
@@ -1,16 +1,34 @@
 package com.disizaniknem.thesocialnetwork.repositories
 
+import com.disizaniknem.thesocialnetwork.data.entities.User
 import com.disizaniknem.thesocialnetwork.other.Resource
+import com.disizaniknem.thesocialnetwork.other.safeCall
 import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class DefaultAuthRepository : AuthRepository {
+
+    val auth = FirebaseAuth.getInstance()
+    val users = FirebaseFirestore.getInstance().collection("users")
 
     override suspend fun register(
         email: String,
         username: String,
         password: String
     ): Resource<AuthResult> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            safeCall {
+                val result = auth.createUserWithEmailAndPassword(email, password).await()
+                val uid = result.user?.uid!!
+                val user = User(uid, username)
+                users.document(uid).set(user).await()
+                Resource.Success(result)
+            }
+        }
     }
 
     override suspend fun login(email: String, password: String): Resource<AuthResult> {
